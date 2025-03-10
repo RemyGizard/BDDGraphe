11a3abce5c9bd9ee69054a2034239f57160a731f
Remy Gizard <remygizard@gmail.com>
2025-03-10 14:42:57 +0100

    fichier aide installation

---
+++ commande_depart.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Initialisation d'OPAM
+echo "Initialisation d'OPAM..."
+opam init -y
+
+# Installation des bibliothèques nécessaires
+echo "Installation des bibliothèques nécessaires..."
+opam install ppx_deriving
+opam install ocamlgraph
+opam install menhir
+opam install uTop
+
+# Configuration de l'environnement OPAM
+echo "Configuration de l'environnement OPAM..."
+eval $(opam env)
+
+# Installation de Dune
+echo "Installation de Dune..."
+opam install dune
+dune init  Proj_GraphDB
+
+# Exécution du projet avec Dune
+echo "Exécution du projet avec Dune..."
+dune exec Proj_GraphDB f test/tiny.q
+
+# Construction du projet
+echo "Construction du projet..."
+dune build
+
+# Lancement de uTop avec Dune
+echo "Lancement de uTop..."
+dune utop
